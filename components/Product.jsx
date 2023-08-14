db8cfe84016f18468a05baadc16a7b64848934c0
Abdullahishtiaq488 <abdullahishtiaq488@gmail.com>
2023-08-15 04:00:47 +0500

    changes

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
-import AddToCartButton from './AddToCartButton'; // Import the new component
+import AddToCartButton from './AddToCartButton'; 
 
 const Product = ({ product }) => {
   const { image, name, slug, price } = product;
 
   return (
     <div>
-      
-        <div className="product-card">
+      <div className="product-card">
         <Link href={`/product/${slug.current}`}>
           <div>
-          <img
-            src={urlFor(image && image[0])}
-            className="product-image"
-          />
-          </div></Link>
-          <p className="product-name">{name}</p>
-          <p className="product-price">${price}</p>
-          
-          <div className='carto-container'>
-          <AddToCartButton product={product} /> {/* Use the new component */}</div>
+            <img
+              src={urlFor(image && image[0])}
+              className="product-image"
+            />
+          </div>
+        </Link>
+        <p className="product-name">{name}</p>
+        <p className="product-price">${price}</p>
+        <div className='carto-container'>
+          <AddToCartButton product={product} />
         </div>
-      
+      </div>
     </div>
   );
 };
